98a39adc5d65b24ff8039cbcc347fbb5f877c468
hahnlee <hanlee.dev@gmail.com>
2018-08-17 00:45:56 +0900

    Updates

--- js/share.js
+++
@@ -1,7 +0,0 @@
-var url = document.URL;
-
-var facebook = document.getElementById("share-link-facebook");
-var twitter = document.getElementById("share-link-twitter");
-
-facebook.href = "https://www.facebook.com/sharer.php?u=" + url;
-twitter.href = "https://twitter.com/intent/tweet?url=" + url;
---
+++ path----2b087140a3508e3fab41.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{104:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Han Lee"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----2b087140a3508e3fab41.js.map
