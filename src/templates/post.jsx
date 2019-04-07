9ca5b9b1236fdf7ad8dff9168d0d8c5ce36279e9
hahnlee <hanlee.dev@gmail.com>
2019-04-08 03:38:39 +0900

    댓글 오류 수정

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -6,24 +6,34 @@ import 'prismjs/themes/prism-okaidia.css'
 import { Page } from '../components'
 import styles from './post.module.scss'
 
+export default class Post extends React.Component {
+  comment = React.createRef()
 
-export default ({ data: { markdownRemark }}) => (
-  <Page title={markdownRemark.frontmatter.title}>
-    <h1 className={styles.title}>{markdownRemark.frontmatter.title}</h1>
-    <p>{markdownRemark.frontmatter.date}</p>
-    <div
-      className={styles.content}
-      dangerouslySetInnerHTML={{__html: markdownRemark.html}}
-    />
-    <script src="https://utteranc.es/client.js"
-      repo="hahnlee/blog"
-      issue-term="pathname"
-      theme="github-light"
-      crossorigin="anonymous"
-      async
-    />
-  </Page>
-)
+  componentDidMount() {
+    const script = document.createElement('script')
+    script.src = 'https://utteranc.es/client.js'
+    script.async = true
+    script.setAttribute('repo', 'hahnlee/blog')
+    script.setAttribute('theme', 'github-light')
+    script.setAttribute('issue-term', 'pathname')
+    this.comment.current.appendChild(script)
+  }
+
+  render() {
+    const { data: { markdownRemark }} = this.props
+    return (
+      <Page title={markdownRemark.frontmatter.title}>
+        <h1 className={styles.title}>{markdownRemark.frontmatter.title}</h1>
+        <p>{markdownRemark.frontmatter.date}</p>
+        <div
+          className={styles.content}
+          dangerouslySetInnerHTML={{ __html: markdownRemark.html }}
+        />
+        <div ref={this.comment} />
+      </Page>
+    )
+  }
+}
 
 export const pageQuery = graphql`
   query($slug: String!) {
