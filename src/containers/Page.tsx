d8babe9768e785f2d239cb88b20c5fd504769118
hahnlee <hanlee.dev@gmail.com>
2021-08-28 18:17:02 +0900

    feat: add favicon

--- src/containers/Page.tsx
+++ src/containers/Page.tsx
@@ -15,7 +15,7 @@ export default function Page({
   type = 'website',
   title,
   description = '기록할 수 있는 지식을 나눕니다',
-  image,
+  image = '/images/og.png',
   publishedTime,
 }: Props) {
   return (
@@ -25,7 +25,7 @@ export default function Page({
         <meta property="og:type" content={type} />
         <meta property="og:title" content={title ?? '명시지(形式知)'} />
         <meta property="og:description" content={description} />
-        {image && <meta property="og:image" content={image} />}
+        <meta property="og:image" content={image} />
         <meta property="og:author" content="이한" />
         <meta
           property="og:site_name"
@@ -43,6 +43,27 @@ export default function Page({
           title="명시지(形式知) - 기록할 수 있는 지식을 나눕니다"
           href="/rss.xml"
         />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" content="#000000" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@400;600&display=swap"
           rel="stylesheet"
